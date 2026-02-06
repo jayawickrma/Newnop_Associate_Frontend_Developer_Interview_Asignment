56c9ca218809690f0fef7406951d74503ccbf459
jayawickrama <nishantharaka2004@gmail.com>
2026-02-06 20:20:27 +0530

    feat (backend-development) : created mongo database and connected using Prisma Client created all models in Backend created Service Classes created Prisma Client to connect with Mongo DB

---
+++ Backend/src/services/issueService.ts
@@ -0,0 +1,23 @@
+import {Issue} from "../models/IssueModel";
+
+class IssueService{
+    async createIssue(issue:Issue){
+
+    }
+
+    async updateIssue(issue:Issue ,id:string){
+
+    }
+
+    async deleteIssue(id:string){
+
+    }
+
+    async getIssueById(id:string){
+
+    }
+
+    async getAllIssues(){}
+}
+const issue_service = new IssueService();
+export default issue_service;
---
+++ Backend/src/services/userService.ts
@@ -0,0 +1,12 @@
+import {User} from "../models/UserModel";
+
+class UserService{
+    async login(req:any,resp:any){
+
+    }
+    async register(user:User){
+
+    }
+}
+const user_service = new UserService();
+export default user_service;
