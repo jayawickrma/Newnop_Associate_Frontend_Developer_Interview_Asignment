1aec80690dfdf5960d11683472e9e034bd59feac
jayawickrama <nishantharaka2004@gmail.com>
2026-02-08 01:07:53 +0530

    Fix (backend/development): Fix Netlify MIME type issue

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,7 @@
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
+  plugins: [react()],
+  base: '/',
 });
